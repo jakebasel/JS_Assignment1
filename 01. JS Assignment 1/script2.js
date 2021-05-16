3d964bc37e39db1765f092e10a76c30ab1797385
jakebasel <83322584+jakebasel@users.noreply.github.com>
2021-05-16 12:16:10 -0700

    Add files via upload

---
+++ 01. JS Assignment 1/script2.js
@@ -0,0 +1,7 @@
+document.write("Assignment Part B<br>");
+for (var i=1;i<100;i++){
+    if (i%2==0){
+        document.write(i);
+        document.write("<br>");
+    }
+}
---
+++ 01. JS Assignment 1/script3.js
@@ -0,0 +1,7 @@
+document.write("Assignment Part C <br>");
+for (var i=1;i<100;i++){
+    if (i%3==0){
+        document.write(i);
+        document.write("<br>");
+    }
+}
